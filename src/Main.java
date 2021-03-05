46cd5f95f1fec7c4d333d2a7b20fd3822d1b4804
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-03-05 11:24:58 -0300

    Creating Department entity class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import model.entities.Department;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1,"books");
+
+        System.out.println(obj);
+    }
+}
---
+++ src/model/entities/Department.java
@@ -0,0 +1,63 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    /*
+    O serializale indica que nossos objetos podem ser transformados em sequências de bytes
+    para que eles possam ser gravados em arquivos, trafegar em rede, etc.
+     */
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer departmentID;
+    private String departmentName;
+
+    public Department() {
+
+    }
+
+    public Department(Integer departmentID, String departmentName) {
+        this.departmentID = departmentID;
+        this.departmentName = departmentName;
+    }
+
+    public Integer getDepartmentID() {
+        return departmentID;
+    }
+
+    public void setDepartmentID(Integer departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return departmentID.equals(that.departmentID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentID);
+    }
+
+    @Override
+    public String toString() {
+        return "Department [" +
+                "departmentID=" + departmentID +
+                ", departmentName='" + departmentName + '\'' +
+                ']';
+    }
+}
